02b6817561971a7bcb16dc29c752b5bc91eecaa8
Ruben Rodriguez <ruben.rodriguez@megaterios.co>
2020-01-14 16:02:51 -0500

    Added view and routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-//Zone to imports
+//imports
 const express = require('express');
 const app = express();
+const path = require('path');
 
+//settings
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
-app.listen(3000, () => {console.log('Server lintening on port', 3000)});
+//middlewares
+
+//routes
+app.use(require('./routes/index'));
+
+//statics files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//server to listening
+app.listen(app.get('port'), () => {console.log('Server lintening on port', app.get('port'))});
